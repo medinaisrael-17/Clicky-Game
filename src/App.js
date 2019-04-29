7fb9d84463b177688a21c59b612a8e6c5dd03ae3
Cabaret Voltaire <medinaisrael_17@yahoo.com>
2019-04-29 16:43:45 -0500

    initial layout for app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    <div>
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <span class="navbar-brand">Clicky Game</span>
+        <div class="collapse navbar-collapse" id="navbarText">
+          <div id="userGuess">
+            <span class="navbar-brand" id="correctGuess">Click An Image to Begin!</span>
+          </div>
+
+          <div id="scoreDiv">
+            <span class="navbar-brand">Score: <span id="currentScore"></span></span>
+
+            <span id="divider" class="navbard-brand">|</span>
+
+            <span class="navbar-brand">Top Score: <span id="topScore"></span> </span>
+          </div>
+        </div>
+      </nav>
+
+      <div class="jumbotron jumbotron-fluid">
+        <div class="container">
+          <h1 class="display-4">Clicky Game!</h1>
+          <p class="lead">Click on an image to earn points, but don't click on any more than once!</p>
+        </div>
+      </div>
+
+      <div id = "gameContainer">
+        {/* <GameContainer /> */}
+      </div>
+
+      <nav class="navbar navbar-light bg-light">
+        <a class="navbar-brand" href="#">
+          <img src="https://cdn4.iconfinder.com/data/icons/logos-3/600/React.js_logo-512.png" width="30" height="30" class="d-inline-block align-top" alt="react"/>
+          MADE WITH REACT
         </a>
-      </header>
+      </nav>
+
     </div>
   );
 }
