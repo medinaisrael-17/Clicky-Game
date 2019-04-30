df21577d3718adb1ce5406e4098cfd2f8ea1ca15
Cabaret Voltaire <medinaisrael_17@yahoo.com>
2019-04-30 15:23:30 -0500

    Updates

---
+++ precache-manifest.2929139fa2c38fddfae31d22f49ee30e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74f8c7ed89ddf7fb5061e667633f52ee",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cdbdffdc66f5a7e5fad7",
+    "url": "/static/css/main.2faf0b5f.chunk.css"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "cdbdffdc66f5a7e5fad7",
+    "url": "/static/js/main.6fac9a97.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
